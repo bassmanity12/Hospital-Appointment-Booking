11515aae8ba38ce8da31f302a470e6e6efef8eb9
bassmanity12 <bassmanity12@gmail.com>
2018-04-02 21:50:11 +0300

    Added login and register pop-ups

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,46 @@
-document.getElementById("signIn")
-    .addEventListener("keypress", function(event) {
-        event.preventDefault();
-        if (event.keyCode === 13) {
-            document.getElementsByClassName("enter").click();
-        }
-    });
+// window.onload = function(){
+//
+//
+// }
+// document.getElementById("signIn")
+//     .addEventListener("keypress", function(event) {
+//         event.preventDefault();
+//         if (event.keyCode === 13) {
+//             document.getElementsByClassName("enter").click();
+//         }
+//     });
+// //
+// function buttonCode()
+// {
+//     alert("Button code executed.");
+// }
+//
+// function ShowDiv() {
+//     document.getElementById("loginForm").style.display = "block";
+window.onload = function () {
+    var loginpopup = document.getElementById(loginpopup);
+    var registerpopup = document.getElementById(registerpopup);
 
-function buttonCode()
-{
-    alert("Button code executed.");
 }
 
-function ShowDiv() {
-    document.getElementById("loginForm").style.display = "block";
+function showloginpopup() {
+
+   loginpopup.style.display="block";
+   console.log("done");
+
+}
+function hideloginpopup() {
+    loginpopup.style.display="none";
+
+}
+function showregisterpopup() {
+
+    registerpopup.style.display="block";
+    console.log("done");
+
+}
+function hideregisterpopup() {
+    registerpopup.style.display="none";
+    console.log("done");
+
 }
